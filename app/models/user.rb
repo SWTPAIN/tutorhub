d75a694b9455d12bb1889dca6f87bc9f4a24d44c
KaFaiC <choikafai2004@gmail.com>
2014-08-16 14:30:29 +0800

    Change jobs belong to Tutor instead of belonging to Student

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  has_many :jobs, foreign_key: "student_id", dependent: :destroy
-  has_many :students, through: :jobs, source: :student
-  has_many :reverse_jobs, foreign_key: "tutor_id",
+  has_many :jobs, foreign_key: "tutor_id", dependent: :destroy
+  has_many :students, through: :jobs
+  has_many :reverse_jobs, foreign_key: "student_id",
                           class_name: "Job",
                           dependent: :destroy
-  has_many :tutors, through: :reverse_jobs, source: :tutor
+  has_many :tutors, through: :reverse_jobs
 
 end
